e69f59c5a3233201c570111b576e09ddda4810b8
tanvi2025 <tanvi.ranga@causecode.com>
2018-07-02 18:46:08 +0530

    Models created

--- main.py
+++ main.py
@@ -1,5 +1,54 @@
 #Im porting libraries
 import tornado.web
-import torando.ioloop
+import tornado.ioloop
 import tornado.httpserver
 import tornado.options
+import sqlalchemy
+from models import User
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import json
+import requests
+
+#Creating session 
+engine=create_engine('sqlite:////Volumes/Others/repos/todolist/users.db', echo=True)
+Session=sessionmaker(bind=engine)
+session=Session()
+
+#Controller class
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        # self.write("Hello world")
+        user=(session.query(User).all())
+        #print(json.load(user))
+        response={
+            'res':'hello'
+        }
+        self.write(response)
+        print(user)
+
+class TasksHandler(tornado.web.RequestHandler):
+    def post(self):
+        taskname=self.get_argument('tasks_name')
+        deadline=self.get_argument('tasks_deadline')
+        print(taskname)
+        print(deadline)
+        #self.write(dict(taskname=task_name),dict(deadline=tasks_deadline))
+        response={
+            'taskname':taskname,
+            'deadline':deadline
+        }
+        self.write(response)
+         
+        
+
+if __name__=="__main__":
+    app = tornado.web.Application(
+    handlers=[(r'/', MainHandler),(r'/tasks',TasksHandler)]
+    
+    #template_path=os.path.join(os.path.dirname(__file__), "templates")
+    ) 
+    portNumber=str(8888)
+    app.listen(portNumber)
+    tornado.ioloop.IOLoop.instance().start()
+    
--- models.py
+++ models.py
@@ -1,23 +1,50 @@
 #Importing sqlalchemy libraries
 import sqlalchemy
-print(sqlalchemy.__version__)
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column,Integer,String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import backref,relationship
+from datetime import datetime
+from sqlalchemy import DateTime
 
 #Connecting db
-engine=create_engine('sqlite:///:memory:', echo=True)
-
+engine=create_engine('sqlite:////Volumes/Others/repos/todolist/users.db', echo=True)
+# engine=create_engine('sqlite:///:memory:', echo=True)
+Session=sessionmaker(bind=engine)
+session=Session()
 #Declarative base
 Base = declarative_base()
 
 #User model
-class UserModel(Base):
-    _tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    password = Column(String)
-     def __repr__(self):
-        return "<User(name='%s', fullname='%s', password='%s')>" % (
-        elf.name, self.fullname, self.password)
+class User(Base):
+    __tablename__='users'
+    user_id=Column(Integer(),primary_key=True)
+    user_name=Column(String(15),nullable=False,unique=True)
+    email_address=Column(String(50),nullable=False)
+    phone=Column(String(20),nullable=False)
+    password=Column(String(25),nullable=False)
+
+    def __repr__(self):
+        return "User(username='{self.user_name}," \
+        "email_address='{self.email_address}' )".format(self=self)
+#Tasks Model 
+class Tasks(Base):
+    __tablename__='tasks'
+
+    tasks_id=Column(Integer(),primary_key=True)
+    tasks_name=Column(String(),nullable=False)
+    tasks_deadline=Column(DateTime(),default=datetime.now)
+    
+    #Representation method
+    def __repr__(self):
+        return "Tasks(tasksname='{self.tasks_name},"\
+                "deadline={'self.tasks_deadline}')".format(self=self)
+
+
+Base.metadata.create_all(engine)
+#Creating the new user record 
+# user=User(user_name='abc',email_address='abc@gmail.com',phone='123456789',password='abc123')
+# session.add(user)
+# session.commit()
+# print('userId:', user.user_id)
